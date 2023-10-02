6e3d21129d4bd04bb60065f4ad11f24f156c93bb
VEDANSH <vedanshgupta09@gmail.com>
2023-10-02 19:08:14 +0530

    import

---
+++ quote-generator/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Quotes from "./components/Quotes";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quote Generator</h1>
+      <div className="cont">
+        <Quotes />
+      </div>
+    </div>
+  );
+}
+
+export default App;
