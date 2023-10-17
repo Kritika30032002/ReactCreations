51338c06c7d2749ae451b1f8fd9b9c5f97dc20d1
Anuj Singh <singhanuj620@gmail.com>
2023-10-17 23:51:37 +0530

    fix(qr-valid-link) : to show QR for only valid web link

--- QR Code Generator/src/Components/QRLoader.js
+++ QR Code Generator/src/Components/QRLoader.js
@@ -4,9 +4,18 @@ import QRCode from 'qrcode.react';
 export default function Qr() {
   const [link, setLink] = useState(''); // State to store user input
   const [qrCodeData, setQRCodeData] = useState(''); // State to store QR code data
+  const [isValidLink, setIsValidLink] = useState(true); // check if link input is valid or not
 
   const generateQRCode = () => {
-    setQRCodeData(link);
+    const checkLink = (/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?/.test(link))
+    if(checkLink){
+      setIsValidLink(true)
+      setQRCodeData(link);
+    }
+    else{
+      setIsValidLink(false)
+    }
+
   };
 
   return (
@@ -20,7 +29,8 @@ export default function Qr() {
       <br/>
       <button onClick={generateQRCode}>Generate new QR</button>
       <br/><br/>
-      {qrCodeData && <QRCode value={qrCodeData} style = {{width: "20rem", height: "20rem"}} />}
+      {qrCodeData && isValidLink && <QRCode value={qrCodeData} style = {{width: "20rem", height: "20rem"}} />}
+      {!isValidLink && <h1>Please enter a valid link !!!</h1>}
     </div>
   );
 }
