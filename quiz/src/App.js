439a7c4808d4e8242a2597b9a212f659ba6cd68a
Anuj Singh <singhanuj620@gmail.com>
2023-10-20 00:29:43 +0530

    fix(quiz-ui) : revamped the quiz app ui

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,80 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import questions from './quizData'
 
-function App() {
-  const [questions, setQuestions] = useState([
-    {
-      question: 'What is the capital of France?',
-      options: ['London', 'Berlin', 'Paris', 'Madrid'],
-      correctAnswer: 'Paris',
-    },
-    {
-      question: 'Which planet is known as the Red Planet?',
-      options: ['Mars', 'Jupiter', 'Earth', 'Venus'],
-      correctAnswer: 'Mars',
-    },
-    {
-      question: 'How many continents are there on Earth?',
-      options: ['4', '5', '6', '7'],
-      correctAnswer: '7',
-    },
-    {
-      question: 'Who wrote the play "Romeo and Juliet"?',
-      options: ['Charles Dickens', 'William Shakespeare', 'Jane Austen', 'F. Scott Fitzgerald'],
-      correctAnswer: 'William Shakespeare',
-    },
-    {
-      question: 'What is the largest mammal in the world?',
-      options: ['African Elephant', 'Blue Whale', 'Giraffe', 'Hippopotamus'],
-      correctAnswer: 'Blue Whale',
-    },
-    {
-      question: 'In which year did Christopher Columbus discover America?',
-      options: ['1492', '1520', '1455', '1510'],
-      correctAnswer: '1492',
-    },
-    {
-      question: 'What is the chemical symbol for gold?',
-      options: ['Au', 'Ag', 'Fe', 'Hg'],
-      correctAnswer: 'Au',
-    },
-    {
-      question: 'Which gas do plants absorb from the atmosphere?',
-      options: ['Oxygen', 'Carbon Dioxide', 'Hydrogen', 'Nitrogen'],
-      correctAnswer: 'Carbon Dioxide',
-    },
-    {
-      question: 'Which country is known as the Land of the Rising Sun?',
-      options: ['China', 'Korea', 'Japan', 'Thailand'],
-      correctAnswer: 'Japan',
-    },
-    {
-      question: 'What is the largest planet in our solar system?',
-      options: ['Mars', 'Jupiter', 'Earth', 'Saturn'],
-      correctAnswer: 'Jupiter',
-    },
-    {
-      question: 'Which animal is known as the "King of the Jungle"?',
-      options: ['Giraffe', 'Tiger', 'Lion', 'Hippopotamus'],
-      correctAnswer: 'Lion',
-    },
-    {
-      question: 'How many bones are there in the adult human body?',
-      options: ['206', '213', '195', '222'],
-      correctAnswer: '206',
-    },
-    {
-      question: 'Who painted the Mona Lisa?',
-      options: ['Pablo Picasso', 'Vincent van Gogh', 'Leonardo da Vinci', 'Rembrandt'],
-      correctAnswer: 'Leonardo da Vinci',
-    },
-    {
-      question: 'Which gas is known as "Laughing Gas"?',
-      options: ['Oxygen', 'Carbon Dioxide', 'Nitrous Oxide', 'Helium'],
-      correctAnswer: 'Nitrous Oxide',
-    },
-  ]);
-
+const App = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [userSelections, setUserSelections] = useState(Array(questions.length).fill(''));
   const [score, setScore] = useState(0);
@@ -139,11 +67,12 @@ function App() {
     if (currentQuestion < questions.length) {
       return (
         <div className="question-container">
-          <p className="question-text">{questions[currentQuestion].question}</p>
+          <p className="display-5">{questions[currentQuestion].question}</p>
           <ul className="options-list">
             {questions[currentQuestion].options.map((option, index) => (
-              <li key={index} className="option-item">
-                <label>
+              <div key={index}>
+              <li className="option-item">
+                <label className='fs-5'>
                   <input
                     type="radio"
                     name="options"
@@ -151,25 +80,41 @@ function App() {
                     onChange={handleOptionChange}
                     checked={userSelections[currentQuestion] === option}
                   />
+                  &nbsp; &nbsp; &nbsp;
                   {option}
                 </label>
               </li>
+              <br></br>
+              </div>
             ))}
           </ul>
-          <div className="timer">Time Remaining: {timer} seconds</div>
-          {result === '' && (
+          <div className="d-flex justify-content-center bg-danger"><div className="text-white">Time Remaining: {timer} seconds</div></div>
+          <br></br>
+          <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar" style={{width: `${((currentQuestion/questions.length)*100)}%`}}></div>
+          </div>
+          <br></br>
+          <div className='d-flex justify-content-around'>
+              <div>
+              {result === '' && (
             <button className="check-answer-button" onClick={checkAnswer}>
               Check Answer
             </button>
           )}
-          {result === 'correct' && <div className="result-mark">&#10004;</div>}
+              {result === 'correct' && <div className="result-mark">&#10004;</div>}
           {result === 'wrong' && <div className="result-mark">&#10006;</div>}
-          <button className="back-button" onClick={moveToPreviousQuestion}>
+              </div>
+              <div>
+              <button className="back-button" onClick={moveToPreviousQuestion}>
             Back
           </button>
-          <button className="next-button" onClick={moveToNextQuestion}>
+              </div>
+              <div>
+              <button className="next-button" onClick={moveToNextQuestion}>
             {currentQuestion === questions.length - 1 ? 'Finish Quiz' : 'Next Question'}
           </button>
+              </div>
+          </div>
         </div>
       );
     } else {
@@ -186,10 +131,22 @@ function App() {
     }
   };
 
+  // return (
+  //   <div className="App">
+  //     <h1 className="quiz-title">Quiz App</h1>
+  //     <div className="quiz">{renderQuiz()}</div>
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <h1 className="quiz-title">Quiz App</h1>
-      <div className="quiz">{renderQuiz()}</div>
+    <div>
+      <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Quiz App</span>
+  </div>
+</nav>
+<div className='d-flex justify-content-center align-items-center quiz-container'>
+  {renderQuiz()}
+</div>
     </div>
   );
 }
--- quiz/src/quizData.js
+++ quiz/src/quizData.js
@@ -1,21 +1,75 @@
-const quizData = [
-    {
-      question: "What is 2 + 2?",
-      options: ["3", "4", "5", "6"],
-      correctAnswer: "4",
-    },
-    {
-      question: "What is the capital of France?",
-      options: ["London", "Berlin", "Paris", "Madrid"],
-      correctAnswer: "Paris",
-    },
-    {
-      question: "Which planet is known as the Red Planet?",
-      options: ["Mars", "Venus", "Earth", "Jupiter"],
-      correctAnswer: "Mars",
-    },
-    // Add more questions here
-  ];
-  
-  export default quizData;
+const questions = [
+  {
+    question: 'What is the capital of France?',
+    options: ['London', 'Berlin', 'Paris', 'Madrid'],
+    correctAnswer: 'Paris',
+  },
+  {
+    question: 'Which planet is known as the Red Planet?',
+    options: ['Mars', 'Jupiter', 'Earth', 'Venus'],
+    correctAnswer: 'Mars',
+  },
+  {
+    question: 'How many continents are there on Earth?',
+    options: ['4', '5', '6', '7'],
+    correctAnswer: '7',
+  },
+  {
+    question: 'Who wrote the play "Romeo and Juliet"?',
+    options: ['Charles Dickens', 'William Shakespeare', 'Jane Austen', 'F. Scott Fitzgerald'],
+    correctAnswer: 'William Shakespeare',
+  },
+  {
+    question: 'What is the largest mammal in the world?',
+    options: ['African Elephant', 'Blue Whale', 'Giraffe', 'Hippopotamus'],
+    correctAnswer: 'Blue Whale',
+  },
+  {
+    question: 'In which year did Christopher Columbus discover America?',
+    options: ['1492', '1520', '1455', '1510'],
+    correctAnswer: '1492',
+  },
+  {
+    question: 'What is the chemical symbol for gold?',
+    options: ['Au', 'Ag', 'Fe', 'Hg'],
+    correctAnswer: 'Au',
+  },
+  {
+    question: 'Which gas do plants absorb from the atmosphere?',
+    options: ['Oxygen', 'Carbon Dioxide', 'Hydrogen', 'Nitrogen'],
+    correctAnswer: 'Carbon Dioxide',
+  },
+  {
+    question: 'Which country is known as the Land of the Rising Sun?',
+    options: ['China', 'Korea', 'Japan', 'Thailand'],
+    correctAnswer: 'Japan',
+  },
+  {
+    question: 'What is the largest planet in our solar system?',
+    options: ['Mars', 'Jupiter', 'Earth', 'Saturn'],
+    correctAnswer: 'Jupiter',
+  },
+  {
+    question: 'Which animal is known as the "King of the Jungle"?',
+    options: ['Giraffe', 'Tiger', 'Lion', 'Hippopotamus'],
+    correctAnswer: 'Lion',
+  },
+  {
+    question: 'How many bones are there in the adult human body?',
+    options: ['206', '213', '195', '222'],
+    correctAnswer: '206',
+  },
+  {
+    question: 'Who painted the Mona Lisa?',
+    options: ['Pablo Picasso', 'Vincent van Gogh', 'Leonardo da Vinci', 'Rembrandt'],
+    correctAnswer: 'Leonardo da Vinci',
+  },
+  {
+    question: 'Which gas is known as "Laughing Gas"?',
+    options: ['Oxygen', 'Carbon Dioxide', 'Nitrous Oxide', 'Helium'],
+    correctAnswer: 'Nitrous Oxide',
+  },
+]
+
+export default questions;
   
