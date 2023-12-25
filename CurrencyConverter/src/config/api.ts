221db8068e36dbeecfdada681036bed1f5bf2b03
Sami3160 <110652247+Sami3160@users.noreply.github.com>
2023-12-26 01:31:02 +0530

    music player final commit

--- CurrencyConverter/src/config/api.ts
+++
@@ -1,5 +0,0 @@
-export const heading: string = "currency converter";
-const API_DOMAIN: string = "https://v6.exchangerate-api.com/v6";
-const API_KEY: string = "cba2e48759299358d2215846";
-export const endpointPath = (from: string): string =>
-  `${API_DOMAIN}/${API_KEY}/latest/${from}`;
