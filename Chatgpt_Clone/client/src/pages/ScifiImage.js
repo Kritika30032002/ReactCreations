cb37c68f5b96a1e2e88e3d726fb7e096541b136d
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2024-02-02 01:16:20 +0530

    feat: Chatgpt clone added
    closes #252

---
+++ Chatgpt_Clone/client/src/pages/ScifiImage.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+import {
+  Box,
+  Typography,
+  useTheme,
+  useMediaQuery,
+  TextField,
+  Button,
+  Alert,
+  Collapse,
+  Card,
+} from "@mui/material";
+
+const ScifiImage = () => {
+  const theme = useTheme();
+  const navigate = useNavigate();
+  //media
+  const isNotMobile = useMediaQuery("(min-width: 1000px)");
+  // states
+  const [text, settext] = useState("");
+  const [image, setImage] = useState("");
+  const [error, setError] = useState("");
+
+  //register ctrl
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/v1/openai/scifi-image", { text });
+      console.log(data);
+      setImage(data);
+    } catch (err) {
+      console.log(error);
+      if (err.response.data.error) {
+        setError(err.response.data.error);
+      } else if (err.message) {
+        setError(err.message);
+      }
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+  return (
+    <Box
+      width={isNotMobile ? "40%" : "80%"}
+      p={"2rem"}
+      m={"2rem auto"}
+      borderRadius={5}
+      sx={{ boxShadow: 5 }}
+      backgroundColor={theme.palette.background.alt}
+    >
+      <Collapse in={error}>
+        <Alert severity="error" sx={{ mb: 2 }}>
+          {error}
+        </Alert>
+      </Collapse>
+      <form onSubmit={handleSubmit}>
+        <Typography variant="h3">Scifi Image</Typography>
+
+        <TextField
+          placeholder="add your text"
+          type="text"
+          multiline={true}
+          required
+          margin="normal"
+          fullWidth
+          value={text}
+          onChange={(e) => {
+            settext(e.target.value);
+          }}
+        />
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          size="large"
+          sx={{ color: "white", mt: 2 }}
+        >
+          Submit
+        </Button>
+        <Typography mt={2}>
+          not this tool ? <Link to="/">GO BACK</Link>
+        </Typography>
+      </form>
+
+      {image ? (
+        <Card
+          sx={{
+            mt: 4,
+            border: 1,
+            boxShadow: 0,
+            height: "500px",
+            borderRadius: 5,
+            borderColor: "natural.medium",
+            bgcolor: "background.default",
+          }}
+        >
+          <Box sx={{ display: "flex", justifyContent: "center", my: 5 }}>
+            <img src={image} alt="scifiimage" />
+          </Box>
+        </Card>
+      ) : (
+        <Card
+          sx={{
+            mt: 4,
+            border: 1,
+            boxShadow: 0,
+            height: "500px",
+            borderRadius: 5,
+            borderColor: "natural.medium",
+            bgcolor: "background.default",
+          }}
+        >
+          <Typography
+            variant="h5"
+            color="natural.main"
+            sx={{
+              textAlign: "center",
+              verticalAlign: "middel",
+              lineHeight: "450px",
+            }}
+          >
+            Your Scifi Image Will Apprea Here
+          </Typography>
+        </Card>
+      )}
+    </Box>
+  );
+};
+
+export default ScifiImage;
